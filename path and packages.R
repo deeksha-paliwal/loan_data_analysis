c4d68dd9ac0e6f00f71a5a38630cc5d8075a25e2
Deeksha Paliwal <deeksha-paliwal@users.noreply.github.com>
2017-08-24 12:01:33 -0400

    Add files via upload

---
+++ path and packages.R
@@ -0,0 +1,22 @@
+path_read <- "/Users/Deeksha/Documents/loan data/home"
+path_write <- "/Users/Deeksha/Documents/loan data/homer"
+
+setwd(path_read)
+
+install.packages('dplyr')
+install.packages('jsonlite')
+install.packages('readr')
+install.packages('tidyr')
+install.packages('ggplot2')
+install.packages('plotly')
+install.packages('cluster')
+install.packages('Hmisc')
+
+library(dplyr)
+library(jsonlite)
+library(readr)
+library(tidyr)
+library(ggplot2)
+library(plotly)
+library(cluster)
+library(Hmisc)
